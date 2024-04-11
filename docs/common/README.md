c54e4f9b458a31a7d4e83e37e61ef4997e08c446
cocoyoon <socozy7210@gmail.com>
2024-04-11 15:37:03 +0900

    type

--- docs/common/README.md
+++ docs/common/README.md
@@ -7,7 +7,3 @@ Handy common modules used across the protocol.
 -   [`jose`](./jose.ts)
 
 Wrapping module for [JOSE](https://www.npmjs.com/package/jose) npm package which provides support for JSON Web Tokens (JWT), JSON Web Signature (JWS), JSON Web Encryption (JWE), JSON Web Key (JWK), JSON Web Key Set (JWKS), and more.
-
--   [`qr`](./qr.ts)
-
-QR Code module for QR Code encode and decode.
--- test/jose.spec.ts
+++ test/jose.spec.ts
@@ -5,15 +5,33 @@ import { compactJws } from "../src/common/jose/index";
 // Test code
 describe("compactJwsWorks", () => {
     test("JWS", async () => {
-        const json_serialized_payload = JSON.stringify({ test: "data" });
-        const payload = new TextEncoder().encode(json_serialized_payload);
-        const privateKey = await jose.generateSecret("Ed25519", {
+        const jwm = JSON.stringify({
+            type: "DIDConnectReq",
+            from: "did:infra:01:PUB_K1_8KeFXUKBR9kctm3eafs2tgqK3XxcqsnHtRp2kjSdfDFSn3x4bK",
+            created_time: 1662441420,
+            expires_time: 1662441435,
+            body: {
+                initiator: {
+                    type: "HOLDER",
+                    serviceEndpoint: "https://wss.infradid.io",
+                    socketId: "12/Ph3SXvXZKCWQFoiwO5Qp",
+                },
+                context: {
+                    domain: "newnal",
+                    action: "connect",
+                },
+            },
+        });
+        const payload = new TextEncoder().encode(jwm);
+        const epk_pair = await jose.generateKeyPair("EdDSA", {
             extractable: true,
         });
-        const jws = await compactJws(payload, privateKey, {
+        console.log(epk_pair);
+        const jws = await compactJws(payload, epk_pair.privateKey, {
             typ: "JWM",
             alg: "EdDSA",
         });
         console.log("Compact JWS created:", jws);
+        await jose.jwtVerify(jws, epk_pair.publicKey);
     });
 });
